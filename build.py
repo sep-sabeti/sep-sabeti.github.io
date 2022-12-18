2331cd09c53a30f54e45b90e5170f25e03e2d95d
Sep Sabeti <63326449+sep-sabeti@users.noreply.github.com>
2022-12-18 15:50:26 -0500

    Init

---
+++ build.py
@@ -0,0 +1,230 @@
+from pybtex.database.input import bibtex
+
+def get_personal_data():
+    name = ["Sepehr", "Sabeti"]
+    email = "ssabeti@uncc.com"
+    github = "sepsabeti"
+    linkedin = "Sepehr-Sabeti"
+    bio_text = f"""
+                <p>I am a data scientist in Surface Transportation Solution of Civil Group at Leidos working on infrastructure asset management program.</p>
+                <!-- <p>
+                    <span style="font-weight: bold;">Research:</span>
+                     I am excited about using emerging technologies, more specifically AL/ML, AR, and VR for solving critical problems in designing, building, maintaining and operation transportation infrastructure. Currently, my research is on worker-centered design of AR interfaces for critical safety systems.
+                     Further, I am big fan of algorithmic and novel solutions for transportation asset management.
+                </p> -->
+                <p>
+                    <span style="font-weight: bold;">Bio:</span> 
+                    I am a PhD candidate at <a href="https://engr.charlotte.edu/" target="_blank"> the William States Lee College of Engineering</a> of <a href="https://charlotte.edu/" target="_blank"> the UNC Charlotte</a> supervised by <a href="https://sites.google.com/uncc.edu/siamlab/people?authuser=0" target="_blank">Omid Shoghli</a>.
+                    During my PhD studies, I joined <a href="https://www.servicenow.com/research/">ServiceNow Research</a> for an internship with the AI platform team, focusing on Human-AI interaction.
+                    I also a MSc in Computer Science from the <a href="https://cci.charlotte.edu/computer-science/45/5" target="_blank">College of Computing and Informatics of UNC Charlotte </a> in 2022 and another MSc in Civil Engineering from the <a href="https://www.topuniversities.com/universities/iran-university-science-technology" target="_blank">Iran University of Science in Technology (IUST) </a> in 2018.
+                    In May 2019, I started my PhD in the <a href="https://ines.charlotte.edu/" target="_blank">Infrastructure and Environmental Systems (INES) </a> program at UNCC. 
+                    In 2022 I joined <a href="https://www.leidos.com/markets/energy/transportation">Leidos</a> Surface Transportation Portfolio in the Civil Group for an internship and subsequently as a data scientist. I have contributed to the development of state-of-the-art highway asset management program at Leidos under the Maintenance Rating Program (MRP), and have closely collaborated with the Virginia Department of Transportation (VDOT) Subject Matter Experts (SMEs) on developing advanced and predictive decision support systems for intelligence maintenance allocation.
+                </p>
+
+                <p>
+                    <span style="font-weight: bold;">Professional Membership:</span> 
+                    I am a member of the Transportation Research Board (TRB) standing committee of Visualization in Transportation (AED80), and a participating member of the AED80 subcomitte on Participatory and Interactive Visualization. I am also
+                    a member of the American Society of Civil Engineers (ASCE) Data Sensing and Analysis (DSA) committee.
+                </p>
+
+
+                <p>For any inquiries, feel free to reach out to me via mail!</p>
+                <p>
+                    <a href="assets/pdf/Sepehr Sabeti - Resume.pdf" target="_blank" style="margin-right: 15px"><i class="fa fa-address-card fa-lg"></i> Resume</a>
+                    <a href="mailto:ssabeti@uncc.edu" style="margin-right: 15px"><i class="far fa-envelope-open fa-lg"></i> Mail</a>
+                    <a href="https://scholar.google.com/citations?user=AYyAsLIAAAAJ&hl=en" target="_blank" style="margin-right: 15px"><i class="fa-solid fa-book"></i> Scholar</a>
+                    <a href="https://github.com/sep-sabeti" target="_blank" style="margin-right: 15px"><i class="fab fa-github fa-lg"></i> Github</a>
+                    <a href="https://www.linkedin.com/in/sepsabeti" target="_blank" style="margin-right: 15px"><i class="fab fa-linkedin fa-lg"></i> LinkedIn</a>
+                </p>
+    """
+    footer = """
+            <div class="col-sm-12" style="">
+                <h4>Credit</h4>
+                <p>
+                    Shoutout to <a href="https://m-niemeyer.github.io/" target="_blank" style="margin-right: 15px"><i class="fab fa-github fa-lg"></i> Michael Niemeyer</a>for creating this tempelate.
+                </p>
+            </div>
+    """
+    return name, bio_text, footer
+
+def get_author_dict():
+    return {
+        'Omidreza Shoghli': 'https://sites.google.com/uncc.edu/siamlab/people?authuser=0',
+        'Mohammadreza Baharani': 'https://mbaharan.github.io/',
+        'Hamed Tabkhi': 'https://coefs.charlotte.edu/htabkhiv/members/hamed-tabkhi/',
+        'Nichole Morris': 'https://www.humanfirst.umn.edu/staff/morris/',
+        'Arash Karimzadeh': 'https://sites.google.com/view/arashkmz'
+
+        }
+
+def generate_person_html(persons, connection=", ", make_bold=True, make_bold_name='Sepehr Sabeti', add_links=True):
+    links = get_author_dict() if add_links else {}
+    s = ""
+    for p in persons:
+        string_part_i = ""
+        for name_part_i in p.get_part('first') + p.get_part('last'): 
+            if string_part_i != "":
+                string_part_i += " "
+            string_part_i += name_part_i
+        if string_part_i in links.keys():
+            string_part_i = f'<a href="{links[string_part_i]}" target="_blank">{string_part_i}</a>'
+        if make_bold and string_part_i == make_bold_name:
+            string_part_i = f'<span style="font-weight: bold";>{make_bold_name}</span>'
+        if p != persons[-1]:
+            string_part_i += connection
+        s += string_part_i
+    return s
+
+def get_paper_entry(entry_key, entry):
+    s = """<div style="margin-bottom: 3em;"> <div class="row"><div class="col-sm-3">"""
+    s += f"""<img src="{entry.fields['img']}" class="img-fluid img-thumbnail" alt="Project image">"""
+    s += """</div><div class="col-sm-9">"""
+
+    if 'award' in entry.fields.keys():
+        s += f"""<a href="{entry.fields['html']}" target="_blank">{entry.fields['title']}</a> <span style="color: red;">({entry.fields['award']})</span><br>"""
+    else:
+        s += f"""<a href="{entry.fields['html']}" target="_blank">{entry.fields['title']}</a> <br>"""
+
+    s += f"""{generate_person_html(entry.persons['author'])} <br>"""
+    s += f"""<span style="font-style: italic;">{entry.fields['booktitle']}</span>, {entry.fields['year']} <br>"""
+
+    # artefacts = {'html': 'Project Page', 'pdf': 'Paper', 'supp': 'Supplemental', 'video': 'Video', 'poster': 'Poster', 'code': 'Code'}
+    artefacts = {'html': 'Project Page', 'pdf': 'Paper'}
+
+    i = 0
+    for (k, v) in artefacts.items():
+        if k in entry.fields.keys():
+            if i > 0:
+                s += ' / '
+            s += f"""<a href="{entry.fields[k]}" target="_blank">{v}</a>"""
+            i += 1
+        else:
+            print(f'[{entry_key}] Warning: Field {k} missing!')
+
+    cite = "<pre><code>@InProceedings{" + f"{entry_key}, \n"
+    cite += "\tauthor = {" + f"{generate_person_html(entry.persons['author'], make_bold=False, add_links=False, connection=' and ')}" + "}, \n"
+    
+    for entr in ['title', 'booktitle','year']:
+        cite += f"\t{entr} = " + "{" + f"{entry.fields[entr]}" + "}, \n"
+    cite += """}</pre></code>"""
+    s += " /" + f"""<button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapse{entry_key}" aria-expanded="false" aria-controls="collapseExample" style="margin-left: -6px; margin-top: -2px;">Expand bibtex</button><div class="collapse" id="collapse{entry_key}"><div class="card card-body">{cite}</div></div>"""
+    s += """ </div> </div> </div>"""
+    return s
+
+def get_talk_entry(entry_key, entry):
+    s = """<div style="margin-bottom: 3em;"> <div class="row"><div class="col-sm-3">"""
+    s += f"""<img src="{entry.fields['img']}" class="img-fluid img-thumbnail" alt="Project image">"""
+    s += """</div><div class="col-sm-9">"""
+    s += f"""{entry.fields['title']}<br>"""
+    s += f"""<span style="font-style: italic;">{entry.fields['booktitle']}</span>, {entry.fields['year']} <br>"""
+
+    artefacts = {'slides': 'Program Website', 'video': 'Recording'}
+    i = 0
+    for (k, v) in artefacts.items():
+        if k in entry.fields.keys():
+            if i > 0:
+                s += ' / '
+            s += f"""<a href="{entry.fields[k]}" target="_blank">{v}</a>"""
+            i += 1
+        else:
+            print(f'[{entry_key}] Warning: Field {k} missing!')
+    s += """ </div> </div> </div>"""
+    return s
+
+def get_publications_html():
+    parser = bibtex.Parser()
+    bib_data = parser.parse_file('/Users/sepsabeti/Downloads/m-niemeyer.github.io-master/publication_list.bib')
+    keys = bib_data.entries.keys()
+    s = ""
+    for k in keys:
+        s+= get_paper_entry(k, bib_data.entries[k])
+    return s
+
+def get_talks_html():
+    parser = bibtex.Parser()
+    bib_data = parser.parse_file('/Users/sepsabeti/Downloads/m-niemeyer.github.io-master/talk_list.bib')
+    keys = bib_data.entries.keys()
+    s = ""
+    for k in keys:
+        s+= get_talk_entry(k, bib_data.entries[k])
+    return s
+
+def get_index_html():
+    pub = get_publications_html()
+    talks = get_talks_html()
+    name, bio_text, footer = get_personal_data()
+    s = f"""
+    <!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+  <title>{name[0] + ' ' + name[1]}</title>
+  <link rel="icon" type="image/x-icon" href="assets/favicon.ico">
+</head>
+
+<body>
+    <div class="container">
+        <div class="row" style="margin-top: 3em;">
+            <div class="col-sm-12" style="margin-bottom: 1em;">
+            <h3 class="display-4" style="text-align: center;"><span style="font-weight: bold;">{name[0]}</span> {name[1]}</h3>
+            </div>
+            <br>
+            <div class="col-md-8" style="">
+                {bio_text}
+            </div>
+            <div class="col-md-4" style="">
+                <img src="assets/img/profile.jpg" class="img-thumbnail" width="280px" alt="Profile picture">
+            </div>
+        </div>
+        <div class="row" style="margin-top: 1em;">
+            <div class="col-sm-12" style="">
+                <h4>Publications</h4>
+                {pub}
+            </div>
+        </div>
+        <div class="row" style="margin-top: 3em;">
+            <div class="col-sm-12" style="">
+                <h4>Talks</h4>
+                {talks}
+            </div>
+        </div>
+        <div class="row" style="margin-top: 3em; margin-bottom: 1em;">
+            {footer}
+        </div>
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+      integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+      crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+      integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+      crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+      integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+      crossorigin="anonymous"></script>
+</body>
+
+</html>
+    """
+    return s
+
+
+def write_index_html(filename='index.html'):
+    s = get_index_html()
+    with open(filename, 'w') as f:
+        f.write(s)
+    print(f'Written index content to {filename}.')
+
+if __name__ == '__main__':
+    write_index_html('/Users/sepsabeti/Downloads/m-niemeyer.github.io-master/index.html')
